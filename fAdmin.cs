187b16e1d03b375dd1cbedc62cc1ac9c72204043
derelehoang <otaku.bokunohana@gmail.com>
2019-11-16 09:07:04 +0700

    no message

---
+++ fAdmin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffee_Managerment
+{
+    public partial class fAdmin : Form
+    {
+        public fAdmin()
+        {
+            InitializeComponent();
+            LoadAccountList();
+        }
+
+        private void tpAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void tpCategoryDrink_Click(object sender, EventArgs e)
+        {
+
+        }
+        void LoadAccountList()
+        {
+            string connectionSTR = @"Data Source=DESKTOP-IKCEBER\PHUONGANH;Initial Catalog=COFFEE_MANAGERMENT;User ID=sa";
+            SqlConnection connection = new SqlConnection(connectionSTR);
+
+            string query = "select * from ACCOUNT";
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(data);
+
+            connection.Close();
+
+            dtgvAccount.DataSource = data;
+        }
+    }
+}
---
+++ fTableManager.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Coffee_Managerment
+{
+    partial class fTableManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngTinTàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngTinCáNhânToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lsvBill = new System.Windows.Forms.ListView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.cbDrink = new System.Windows.Forms.ComboBox();
+            this.btnAddDrink = new System.Windows.Forms.Button();
+            this.nmDrink = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnGiamGia = new System.Windows.Forms.Button();
+            this.nmCount = new System.Windows.Forms.NumericUpDown();
+            this.btnSwichTable = new System.Windows.Forms.Button();
+            this.cbSwichTable = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmDrink)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminToolStripMenuItem,
+            this.thôngTinTàiKhoảnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1074, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(77, 29);
+            this.adminToolStripMenuItem.Text = "Admin";
+            this.adminToolStripMenuItem.Click += new System.EventHandler(this.adminToolStripMenuItem_Click);
+            // 
+            // thôngTinTàiKhoảnToolStripMenuItem
+            // 
+            this.thôngTinTàiKhoảnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thôngTinCáNhânToolStripMenuItem,
+            this.đăngXuấtToolStripMenuItem});
+            this.thôngTinTàiKhoảnToolStripMenuItem.Name = "thôngTinTàiKhoảnToolStripMenuItem";
+            this.thôngTinTàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(178, 29);
+            this.thôngTinTàiKhoảnToolStripMenuItem.Text = "Thông tin tài khoản";
+            // 
+            // thôngTinCáNhânToolStripMenuItem
+            // 
+            this.thôngTinCáNhânToolStripMenuItem.Name = "thôngTinCáNhânToolStripMenuItem";
+            this.thôngTinCáNhânToolStripMenuItem.Size = new System.Drawing.Size(238, 30);
+            this.thôngTinCáNhânToolStripMenuItem.Text = "Thông tin cá nhân";
+            this.thôngTinCáNhânToolStripMenuItem.Click += new System.EventHandler(this.thôngTinCáNhânToolStripMenuItem_Click);
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(238, 30);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            this.đăngXuấtToolStripMenuItem.Click += new System.EventHandler(this.đăngXuấtToolStripMenuItem_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lsvBill);
+            this.panel2.Location = new System.Drawing.Point(608, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(454, 409);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.nmCount);
+            this.panel3.Controls.Add(this.btnSwichTable);
+            this.panel3.Controls.Add(this.cbSwichTable);
+            this.panel3.Controls.Add(this.btnGiamGia);
+            this.panel3.Controls.Add(this.btnThanhToan);
+            this.panel3.Location = new System.Drawing.Point(608, 530);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(454, 70);
+            this.panel3.TabIndex = 3;
+            // 
+            // lsvBill
+            // 
+            this.lsvBill.Location = new System.Drawing.Point(0, 3);
+            this.lsvBill.Name = "lsvBill";
+            this.lsvBill.Size = new System.Drawing.Size(454, 403);
+            this.lsvBill.TabIndex = 0;
+            this.lsvBill.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.nmDrink);
+            this.panel4.Controls.Add(this.btnAddDrink);
+            this.panel4.Controls.Add(this.cbDrink);
+            this.panel4.Controls.Add(this.cbCategory);
+            this.panel4.Location = new System.Drawing.Point(608, 36);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(454, 76);
+            this.panel4.TabIndex = 4;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(3, 0);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(245, 28);
+            this.cbCategory.TabIndex = 0;
+            // 
+            // cbDrink
+            // 
+            this.cbDrink.FormattingEnabled = true;
+            this.cbDrink.Location = new System.Drawing.Point(3, 34);
+            this.cbDrink.Name = "cbDrink";
+            this.cbDrink.Size = new System.Drawing.Size(245, 28);
+            this.cbDrink.TabIndex = 0;
+            // 
+            // btnAddDrink
+            // 
+            this.btnAddDrink.Location = new System.Drawing.Point(254, 3);
+            this.btnAddDrink.Name = "btnAddDrink";
+            this.btnAddDrink.Size = new System.Drawing.Size(95, 59);
+            this.btnAddDrink.TabIndex = 1;
+            this.btnAddDrink.Text = "Add";
+            this.btnAddDrink.UseVisualStyleBackColor = true;
+            // 
+            // nmDrink
+            // 
+            this.nmDrink.Location = new System.Drawing.Point(378, 20);
+            this.nmDrink.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nmDrink.Name = "nmDrink";
+            this.nmDrink.Size = new System.Drawing.Size(76, 26);
+            this.nmDrink.TabIndex = 2;
+            this.nmDrink.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 36);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(590, 564);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(356, 3);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(95, 59);
+            this.btnThanhToan.TabIndex = 1;
+            this.btnThanhToan.Text = "Thanh Toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btnGiamGia
+            // 
+            this.btnGiamGia.Location = new System.Drawing.Point(184, 3);
+            this.btnGiamGia.Name = "btnGiamGia";
+            this.btnGiamGia.Size = new System.Drawing.Size(109, 32);
+            this.btnGiamGia.TabIndex = 1;
+            this.btnGiamGia.Text = "Giảm giá";
+            this.btnGiamGia.UseVisualStyleBackColor = true;
+            // 
+            // nmCount
+            // 
+            this.nmCount.Location = new System.Drawing.Point(184, 36);
+            this.nmCount.Name = "nmCount";
+            this.nmCount.Size = new System.Drawing.Size(109, 26);
+            this.nmCount.TabIndex = 2;
+            this.nmCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSwichTable
+            // 
+            this.btnSwichTable.Location = new System.Drawing.Point(3, 3);
+            this.btnSwichTable.Name = "btnSwichTable";
+            this.btnSwichTable.Size = new System.Drawing.Size(109, 32);
+            this.btnSwichTable.TabIndex = 1;
+            this.btnSwichTable.Text = "Chuyển bàn";
+            this.btnSwichTable.UseVisualStyleBackColor = true;
+            // 
+            // cbSwichTable
+            // 
+            this.cbSwichTable.FormattingEnabled = true;
+            this.cbSwichTable.Location = new System.Drawing.Point(3, 36);
+            this.cbSwichTable.Name = "cbSwichTable";
+            this.cbSwichTable.Size = new System.Drawing.Size(109, 28);
+            this.cbSwichTable.TabIndex = 0;
+            // 
+            // fTableManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 612);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fTableManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "phần mềm quản lý coffee";
+            this.Load += new System.EventHandler(this.fTableManager_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nmDrink)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngTinTàiKhoảnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngTinCáNhânToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView lsvBill;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.NumericUpDown nmCount;
+        private System.Windows.Forms.Button btnSwichTable;
+        private System.Windows.Forms.ComboBox cbSwichTable;
+        private System.Windows.Forms.Button btnGiamGia;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.NumericUpDown nmDrink;
+        private System.Windows.Forms.Button btnAddDrink;
+        private System.Windows.Forms.ComboBox cbDrink;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
